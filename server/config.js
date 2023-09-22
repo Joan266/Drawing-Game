3c4b553013992e4fde4ef526c8341b9c426366e6
Joan Alemany <joanalemany26@gmail.com>
2023-09-22 19:33:11 +0100

    update dependencies

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
-import { config } from "dotenv";// import rwe from "random-words-es";
+import { config } from "dotenv";
+
 config(); // Load environment variables from .env file
 
-export const MONGODB_URI = process.env.MONGODB_URI;
-export const PORT = process.env.PORT || 8000; // Use the value of the PORT environment variable, or default to 8000
-
-
+export const { MONGODB_URI } = process.env;
+export const PORT = process.env.PORT || 8000; 
+// Use the value of the PORT environment variable, or default to 8000
