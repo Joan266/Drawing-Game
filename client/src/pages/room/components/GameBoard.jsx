6c2454e0677b71c8f56c455f8f36a2b055bd5a63
Joan Alemany <joanalemany26@gmail.com>
2023-10-31 11:11:57 +0000

    refactored GameBoard component

--- client/src/pages/room/components/GameBoard.jsx
+++ client/src/pages/room/components/GameBoard.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState, useRef, useReducer } from 'react';
-import { usePlayerContext, useGameContext, useRoomContext,useSetPlayerContext } from "../context";
-
+import React, { useEffect, useRef, useReducer } from 'react';
+import { usePlayerContext, useGameContext, useRoomContext } from "../context";
 
 const Canvas = () => {
   const canvasRef = useRef(null);
   const { socket } = useRoomContext();
   const { isGamePlaying, gamePhase } = useGameContext();
-  
+  const { artistTurn } = usePlayerContext();
+
   const canvasReducer = (state, action) => {
     switch (action.type) {
       case 'ADD_COORDINATES':
-        if (state.isDrawing && isGamePlaying && gamePhase === 'guess') {
+        if (state.isMouseDown && isGamePlaying && gamePhase === 'guess') {
           return { ...state, coordinatesBuffer: [...state.coordinatesBuffer, ...action.coordinatesBuffer] };
         }
         return state;
@@ -18,118 +18,121 @@ const Canvas = () => {
         return { ...state, coordinatesBuffer: [] };
       case 'IS_DRAWING':
         if (isGamePlaying && gamePhase === 'guess') {
-          return { ...state, isDrawing: true };
+          return { ...state, isMouseDown: true };
         }
         return state;
       case 'IS_NOT_DRAWING':
-        return { ...state, isDrawing: false };
+        return { ...state, isMouseDown: false };
       default:
         return state;
     }
-  };  
-  
+  };
+
   const initialState = {
-    isDrawing: false,
+    isMouseDown: false,
     coordinatesBuffer: [],
   };
 
   const [state, dispatch] = useReducer(canvasReducer, initialState);
-  const { isDrawing, coordinatesBuffer } = state;
+  const { isMouseDown, coordinatesBuffer } = state;
+  const intervalRef = useRef(null);
+
   const setUpCanvas = () => {
     const canvas = canvasRef.current;
     const canvasContainer = canvas.parentElement; // Parent container for dynamic sizing
-    const { devicePixelRatio: ratio = 1 } = window;
     const context = canvas.getContext('2d');
-  
+
     // Function to resize the canvas based on the container's size
     const resizeCanvas = () => {
       const { width, height } = canvasContainer.getBoundingClientRect();
-      canvas.width = width * ratio;
-      canvas.height = height * ratio;
+      canvas.width = width;
+      canvas.height = height;
     };
-  
+
     // Set initial canvas size
     resizeCanvas();
-  
+
     // Define initial styles
-    context.scale(ratio, ratio);
     context.strokeStyle = 'black';
     context.fillStyle = 'black';
     context.lineWidth = 7;
-  
+
     // Event listener for resizing the canvas dynamically
     window.addEventListener('resize', resizeCanvas);
-  
-    return { canvas, context, resizeCanvas };
-  };
-  
 
-  const canvasDrawing = (coordinatesBuffer) => {
-    const { context } = setUpCanvas();
-    context.beginPath();
-    context.arc(coordinatesBuffer[0][0], coordinatesBuffer[0][1], 3.2, 0, Math.PI * 2, true);
-    context.fill();
-    for (let i = 0; i < coordinatesBuffer.length - 1; i++) {
-      const current_point = coordinatesBuffer[i];
-      const next_point = coordinatesBuffer[i + 1];
-      // Move to the initial point (no line)
-      context.beginPath();
-      context.moveTo(current_point[0], current_point[1]);
-      context.arc(next_point[0], next_point[1], 3.2, 0, Math.PI * 2, true);
-      context.lineTo(next_point[0], next_point[1]);
-      context.stroke();
-      context.fill();
-    }
+    return { canvas, context };
   };
 
   useEffect(() => {
     const emitCoordinates = () => {
-      if ( isDrawing ) {
+      if (artistTurn && isMouseDown && gamePhase === "guess") {
         socket.emit('canvas:mousemove', coordinatesBuffer);
-        dispatch({ type: 'ADD_COORDINATES', coordinatesBuffer: coordinatesBuffer.pop() });
       }
     };
 
-    setinterval(emitcoordinates every 300 milisecons)
+    if (!intervalRef.current) {
+      // Set up the interval when the component mounts
+      intervalRef.current = setInterval(emitCoordinates, 300);
+    }
+
     return () => {
-      clearinterval
+      // Clear the interval when the component unmounts
+      clearInterval(intervalRef.current);
     };
-  },[]);
+  }, [socket, isMouseDown, coordinatesBuffer, gamePhase, artistTurn]);
+
   useEffect(() => {
-    const { canvas } = setUpCanvas();
+    const canvasDrawing = (coordinatesBufferReceived, context) => {
+      context.beginPath();
+      context.arc(coordinatesBufferReceived[0].x, coordinatesBufferReceived[0].y, 3.2, 0, Math.PI * 2, true);
+      context.fill();
+      for (let i = 0; i < coordinatesBufferReceived.length - 1; i++) {
+        const current_point = coordinatesBufferReceived[i];
+        const next_point = coordinatesBufferReceived[i + 1];
+        context.beginPath();
+        context.moveTo(current_point.x, current_point.y);
+        context.arc(next_point.x, next_point.y, 3.2, 0, Math.PI * 2, true);
+        context.lineTo(next_point.x, next_point.y);
+        context.stroke();
+        context.fill();
+      }
+    };
 
     const handleMouseDown = (eventCoordinates) => {
-        const { x, y } = eventCoordinates;
-        dispatch({ type: 'IS_DRAWING' });
-        dispatch({ type: 'ADD_COORDINATES', coordinatesBuffer: [...coordinatesBuffer, { x, y }] });
+      dispatch({ type: 'IS_DRAWING' });
+      dispatch({ type: 'ADD_COORDINATES', coordinatesBuffer: [eventCoordinates] });
+      socket.emit('canvas:drawing', [eventCoordinates]);
     };
 
     const handleMouseMove = (eventCoordinates) => {
-      const { x, y } = eventCoordinates;
-      dispatch({ type: 'ADD_COORDINATES', coordinatesBuffer: [...coordinatesBuffer, { x, y }] });
+      if (isMouseDown) {
+        dispatch({ type: 'ADD_COORDINATES', coordinatesBuffer: [eventCoordinates] });
+      }
     };
 
     const handleMouseUp = () => {
-      if ( isDrawing ) {
-        socket.emit('canvas:mousemove', coordinatesBuffer);
+      if (isMouseDown) {
+        socket.emit('canvas:drawing', coordinatesBuffer);
+        dispatch({ type: 'IS_NOT_DRAWING' });
+        dispatch({ type: 'CLEAR_COORDINATES_BUFFER' });
       }
-      dispatch({ type: 'IS_NOT_DRAWING' });
-      dispatch({ type: 'CLEAR_COORDINATES_BUFFER' });
     };
 
-    canvas.addEventListener('mousedown', handleMouseDown);
-    canvas.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
+    if (artistTurn && gamePhase === "guess") {
+      const { canvas, context } = setUpCanvas();
+      canvas.addEventListener('mousedown', (e) => handleMouseDown({ x: e.clientX, y: e.clientY }));
+      canvas.addEventListener('mousemove', (e) => handleMouseMove({ x: e.clientX, y: e.clientY }));
+      document.addEventListener('mouseup', handleMouseUp);
+      socket.on('canvas:drawing', (data) => canvasDrawing(data, context));
 
-    socket.on('canvas:drawing', canvasDrawing);
-
-    return () => {
-      canvas.removeEventListener('mousedown', handleMouseDown);
-      canvas.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-      socket.off('canvas:drawing', canvasDrawing);
-    };
-  }, [socket, isDrawing, coordinatesBuffer, isGamePlaying, gamePhase]);
+      return () => {
+        canvas.removeEventListener('mousedown', handleMouseDown);
+        canvas.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+        socket.off('canvas:drawing', canvasDrawing);
+      };
+    }
+  }, [socket, isMouseDown, coordinatesBuffer, gamePhase, artistTurn]);
 
   return (
     <div className="canvasContainer">
